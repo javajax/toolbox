e1fc21115342fd722cf278f7282423aa39c262ed
Jack Thorne <jthorne@where.com>
2013-06-13 15:50:04 -0400

    fixing concurrency issues

--- test_manipulator.rb
+++ test_manipulator.rb
@@ -1,18 +1,13 @@
 require 'rubygems'
-#require 'celluloid'
 require 'ruby-debug'
 require 'uuid'
 require 'tempfile'
 
 class FileManipulator
-  # include Celluloid
-
-  def initialize(file)
+  def process(file)
     @file = File.new(file, "r+")
-  end
-
-  def process()
     puts "START #{@file.path}"
+
     @text = []
     @file.each_line{ |line| @text << line }
 
@@ -27,13 +22,16 @@ class FileManipulator
 
   def test_change(index, replace_word)
     puts "TESTING:#{replace_word.upcase}:#{@text[index].lstrip}"
-    tmp_file = Tempfile.new(UUID.new.generate)
+    tmp_file = Tempfile.new( UUID.generate )
 
     test_text = @text
     test_text[index].sub('create', replace_word)
     tmp_file.write(test_text.join("\n"))
 
-    system("ruby -Itest:lib #{tmp_file.path}")
+    `ruby -Itest:lib #{tmp_file.path}`
+
+    puts tmp_file.size
+    puts $?.exitstatus
 
     if $?.exitstatus  == 0
       commit_change(test_text, "#{replace_word.upcase}:#{test_text[index].lstrip}")
@@ -48,7 +46,7 @@ class FileManipulator
       @file.rewind
       @file.truncate(0)
       @file.write(@text.join("\n"))
-      `git commit -am "#{@file.path}:#{message}"`
+      #`git commit -am "#{@file.path}:#{message}"`
   end
 
 end
@@ -56,10 +54,7 @@ end
 files = `find test | grep _test`
 puts files
 
-#FileManipulator.new('test/unit/ad_details_test.rb').process()
-
-pool = FileManipulator.pool(size: 6)
-files.split("\n").each{ |file| pool.future(:process, file) }
-=begin
-=end
+files.split("\n").each do  |file|
+  FileManipulator.new.process(file)
+end
 
